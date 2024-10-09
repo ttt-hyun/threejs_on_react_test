9565f0208c4a5d4a9f8364dd6d07749086fe31da
박태현 <taehyoun330@gmail.com>
2024-10-09 11:59:21 +0900

    first work with sangwon

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,17 +4,24 @@ import {
   Float,
   OrbitControls,
 } from "@react-three/drei";
+import { Hood } from "./Hood";
+import { Hood_normal } from "./Hood_normal";
+import { Hood_normal2 } from "./Hood_normal2";
 import { Mug } from "./Mug";
 
 export const Experience = () => {
   return (
     <>
+        {/* <Hood />
+        <Mug /> */}
       <OrbitControls />
       <Float>
-        <Mug />
+        {/* <Hood /> */}
+        {/* <Mug /> */}
+        <Hood_normal />
       </Float>
       <ContactShadows position-y={-0.5} opacity={0.4} blur={2} />
-      <Environment preset="sunset" background blur={4} />
+      <Environment preset="forest" background blur={4} />
     </>
   );
 };
